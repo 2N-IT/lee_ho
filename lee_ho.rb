56df2f0ae0ce1efa79aca2bea207c22b2623a717
Adam Piotrowski <adamsarin@gmail.com>
2020-05-17 19:41:20 +0200

    basic flow implemented

---
+++ lee_ho.rb
@@ -0,0 +1,18 @@
+module Skipper
+  class LeeHo
+    def call
+      Crew.ready_about('lee_ho')
+      SteermanTurn.call('left')
+      MizenSailPull.call
+      LeftJibSheetRellease.call
+      MainsailRelease.call
+      MizzenSailRelease.call
+      MizzenSailHaul.call
+      MainSailHaul.call
+      ForesailHaul.call
+      SteerKeepHerSo.call
+      SteerSteady.call
+    end
+  end
+end
+
